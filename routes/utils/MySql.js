49421c0116d0a103fb7427d5d3291873a71c9fe9
razanjbara <127765142+razan2704@users.noreply.github.com>
2024-09-25 19:51:22 +0300

    Update MySql.js

--- routes/utils/MySql.js
+++ routes/utils/MySql.js
@@ -1,53 +1,51 @@
-var mysql = require('mysql');
+const mysql = require('mysql2/promise');
 require("dotenv").config();
 
-
-const config={
-connectionLimit:4,
-  host: process.env.host,//"localhost"
-  user: process.env.user,//"root"
-  password: "pass_root@123",
-  database:"mydb"
-}
-const pool = new mysql.createPool(config);
-
-const connection =  () => {
-  return new Promise((resolve, reject) => {
-  pool.getConnection((err, connection) => {
-    if (err) reject(err);
-    console.log("MySQL pool connected: threadId " + connection.threadId);
-    const query = (sql, binding) => {
-      return new Promise((resolve, reject) => {
-         connection.query(sql, binding, (err, result) => {
-           if (err) reject(err);
-           resolve(result);
-           });
-         });
-       };
-       const release = () => {
-         return new Promise((resolve, reject) => {
-           if (err) reject(err);
-           console.log("MySQL pool released: threadId " + connection.threadId);
-           resolve(connection.release());
-         });
-       };
-       resolve({ query, release });
-     });
-   });
- };
-const query = (sql, binding) => {
-  return new Promise((resolve, reject) => {
-    pool.query(sql, binding, (err, result, fields) => {
-      if (err) reject(err);
-      resolve(result);
-    });
-  });
+const config = {
+  connectionLimit: 4,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'pass_root@123',
+  database: process.env.DB_NAME || 'mydb'
 };
+
+// יצירת חיבור ברמת ה- pool 
+const pool = mysql.createPool(config);
+
+// פונקציה להרצת שאילתות
+const query = async (sql, binding) => {
+  try {
+    const [rows] = await pool.query(sql, binding);
+    console.log("Query executed successfully.");
+    return results;
+  } catch (err) {
+    console.error("Query failed:", err);
+    throw err;
+  }
+};
+
+// פונקציה לקבלת חיבור
+const connection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("MySQL pool connected: threadId " + connection.threadId);
+    
+    // הפונקציה query והרליסינג 
+    const query = async (sql, binding) => {
+      const [results] = await connection.query(sql, binding);
+      return results;
+    };
+
+    const release = () => {
+      console.log("MySQL pool released: threadId " + connection.threadId);
+      connection.release();
+    };
+
+    return { query, release };
+  } catch (err) {
+    console.error("Error connecting to MySQL:", err);
+    throw err;
+  }
+};
+
 module.exports = { pool, connection, query };
-
-
-
-
-
-
-
